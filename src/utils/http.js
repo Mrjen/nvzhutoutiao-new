e0e6adfac8da0f993714fe49464ee88c292dcf2d
Unknown <improveselfchen@gmail.com>
2018-06-09 11:02:10 +0800

    备份

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,25 +1,24 @@
-export default function wxRequest(url, params = {}, method = "GET") {
+export default async function wxRequest(url, params = {}, method = "GET") {
+  if (typeof (params) === 'object') {
+    if (!params.token){
+       params.token = wx.getStorageSync("token") || "";
+    }
+  }
 
-                 if(typeof(params)==='object'){
-                    if(!params.token){
-                      params.token = wx.getStorageSync("token") || "";
-                    }                  
-                 }
-                //  console.log(params);
-                 return new Promise(function(resolve, reject) {
-                   wx.request({
-                     url: url,
-                     data: params,
-                     method: method ? method : "GET",
-                     header: {
-                      'content-type': 'application/json'
-                     },
-                     success: res => {
-                       resolve(res);
-                     },
-                     fail: res => {
-                       reject(res);
-                     }
-                   });
-                 });
-               }
+  return new Promise(function (resolve, reject) {
+    wx.request({
+      url: url,
+      data: params,
+      method: method ? method : "GET",
+      header: {
+        'content-type': 'application/json'
+      },
+      success: res => {
+        resolve(res);
+      },
+      fail: res => {
+        reject(res);
+      }
+    });
+  });
+}
